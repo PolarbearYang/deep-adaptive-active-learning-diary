55f085b8908db828c93b3eeba70beed7426314d7
PolarbearYang <1434367219@qq.com>
2021-08-19 16:57:41 +0800

    Updated

--- conf.py
+++ conf.py
@@ -35,6 +35,7 @@ release = 'v0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+   'myst_parser',
    'sphinx.ext.autodoc',
    'sphinx.ext.doctest',
    'sphinx.ext.intersphinx',
